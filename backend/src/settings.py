211cfc1f46a685266f7eec25903f58ea1890da93
katsuyorii <katsuyoriiii@gmail.com>
2025-06-06 15:51:02 +0300

    Create and initial setup project

---
+++ backend/src/settings.py
@@ -0,0 +1,51 @@
+from pydantic import EmailStr
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class CustomBaseSettings(BaseSettings):
+    model_config = SettingsConfigDict(env_file='../.env', extra='allow')
+
+class DatabaseSettings(CustomBaseSettings):
+    DB_HOST: str
+    DB_PORT: str
+    DB_USER: str
+    DB_PASSWORD: str
+    DB_NAME: str
+
+    @property
+    def DATABASE_URL(self) -> str:
+        return f'postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}'
+
+class RedisSettings(CustomBaseSettings):
+    REDIS_HOST: str
+    REDIS_PORT: str
+    REDIS_PASSWORD: str
+    REDIS_POOL_SIZE: int
+
+class RabbitMQSettings(CustomBaseSettings):
+    RABBITMQ_DEFAULT_USER: str
+    RABBITMQ_DEFAULT_PASS: str
+    RABBITMQ_HOST: str
+    RABBITMQ_PORT: int
+
+    @property
+    def RABBIT_MQ_URL(self) -> str:
+        return f"amqp://{self.RABBITMQ_DEFAULT_USER}:{self.RABBITMQ_DEFAULT_PASS}@{self.RABBITMQ_HOST}:{self.RABBITMQ_PORT}//"
+
+class SMTPSettings(CustomBaseSettings):
+    SMTP_HOST: str
+    SMTP_PORT: int
+    SMTP_USER: EmailStr
+    SMTP_PASSWORD: str
+
+class JWTSettings(CustomBaseSettings):
+    SECRET_KEY: str
+    ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
+    REFRESH_TOKEN_EXPIRE_DAYS: int
+
+database_settings = DatabaseSettings()
+redis_settings = RedisSettings()
+rabbit_mq_settings = RabbitMQSettings()
+smtp_settings = SMTPSettings()
+jwt_settings = JWTSettings()
